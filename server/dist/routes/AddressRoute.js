c7d336d25e748314eeb1ed8922c5670fe64aa7cb
Syed Muhemin Ali <59026436+syedalihussain100@users.noreply.github.com>
2026-03-24 20:31:11 +0500

    updated code

--- server/dist/routes/AddressRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { protect } from "../middleware/Auth.js";
-import { addAddresses, DeleteAddresses, getAddresses, updateAddresses } from "../controllers/addressController.js";
-const AddressRoute = express.Router();
-AddressRoute.get("/", protect, getAddresses);
-AddressRoute.get("/", protect, addAddresses);
-AddressRoute.get("/:id", protect, updateAddresses);
-AddressRoute.get("/:id", protect, DeleteAddresses);
-export default AddressRoute;
