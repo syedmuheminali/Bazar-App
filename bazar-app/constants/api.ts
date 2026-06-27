0d8066c2c93e967aecf6de0de12c23d8d9856b20
Syed Muhemin Ali <59026436+syedalihussain100@users.noreply.github.com>
2026-06-27 17:26:07 +0500

    working

--- bazar-app/constants/api.ts
+++ bazar-app/constants/api.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 import { Platform } from "react-native";
 
-const LOCAL_API_URL = Platform.select({
-    android: "http://192.168.0.107:3000/api",
-    ios: "http://192.168.0.107:3000/api",
-    default: "http://localhost:3000/api"
-})
+const API_URL = __DEV__
+  ? Platform.select({
+      android: "http://192.168.0.107:3000/api",
+      ios: "http://192.168.0.107:3000/api",
+      default: "http://localhost:3000/api"
+    })
+  : "https://bazar-app-dusky.vercel.app/api";
 
-
-
-const api = axios.create({baseURL: LOCAL_API_URL});
-
+const api = axios.create({
+  baseURL: API_URL
+});
 
 export default api;
